2ebe03402b9a2205921577990749877cb5e02e6a
benomase <benomarseifeddine@gmail.com>
2017-09-12 21:52:23 +0200

    ejb is now working with JPA , and JPA is communicating with MYSQL

--- ejbModule/com/entities/Room.java
+++ ejbModule/com/entities/Room.java
@@ -7,12 +7,29 @@ import javax.persistence.*;
  * Entity implementation class for Entity: Room
  *
  */
-@Entity
 
+@Entity
 public class Room implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nom;
+	
+	public Long getId() {
+		return id;
+	}	
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
 
 	public Room() {
 		super();
---
+++ ejbModule/com/logic/DbRemote.java
@@ -0,0 +1,11 @@
+package com.logic;
+
+import javax.ejb.Remote;
+
+import com.entities.Room;
+
+@Remote
+public interface DbRemote {
+	
+	public java.util.List<Room> getAllRooms();
+}
