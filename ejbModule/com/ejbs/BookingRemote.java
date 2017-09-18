3a2d2f54cc78894d7c6bb2660ce20ba4e6a83c94
benomase <benomarseifeddine@gmail.com>
2017-09-18 12:12:30 +0200

    n

--- ejbModule/com/ejbs/BookingRemote.java
+++ ejbModule/com/ejbs/BookingRemote.java
@@ -8,16 +8,5 @@ import com.entities.Chambre;
 
 @Remote
 public interface BookingRemote {
-	
-	public List<Chambre> getALlRooms(
-			//a verifier ce qu'on a besoin comme addresse avec Hamada
-//			String postCode,
-//			String state,
-//			String city,
-//			String checkInDate,
-//			String checkOutDate,
-//			String nbRoom,
-//			String nbAdult,
-//			String nbChildren
-			);
+
 }
---
+++ ejbModule/com/ejbs/HotelsRemote.java
@@ -0,0 +1,23 @@
+package com.ejbs;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import com.entities.Chambre;
+
+@Remote
+public interface HotelsRemote {
+	
+	public List<Chambre> getALlRooms(
+			//a verifier ce qu'on a besoin comme addresse avec Hamada
+//			String postCode,
+//			String state,
+//			String city,
+//			String checkInDate,
+//			String checkOutDate,
+//			String nbRoom,
+//			String nbAdult,
+//			String nbChildren
+			);
+}
