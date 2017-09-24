916856176f29e683b182bd445e69a954d38eb2a7
karim <abdelkarim.drareni@hotmail.com>
2017-09-24 14:43:39 +0200

    client reservation done

--- ejbModule/com/ejbs/WishListRemote.java
+++ ejbModule/com/ejbs/WishListRemote.java
@@ -1,8 +1,16 @@
 package com.ejbs;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import com.entities.Reservation;
 
 @Remote
 public interface WishListRemote {
+
+	List<Reservation> getWishList();
+
+	List<Reservation> getWishListForClient(Long idClient);
 
 }
